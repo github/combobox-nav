0cd9d9d9cc0fdb088cda853c0cd75369498cbd9f
Kristján Oddsson <koddsson@gmail.com>
2019-09-11 13:15:45 +0100

    add typescript definition file

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export function install(input: HTMLTextAreaElement | HTMLInputElement, list: HTMLElement): void;
+export function navigate(input: HTMLTextAreaElement | HTMLInputElement, list: HTMLElement, indexDiff?: -1 | 1): void;
+export function uninstall(input: HTMLTextAreaElement | HTMLInputElement, list: HTMLElement): void;
+export function clearSelection(input: HTMLTextAreaElement | HTMLInputElement, list: HTMLElement): void;
